9e1c432dc9a6781ad9cde74e675125ee637bf61d
jasperknott <30095212@tafe.wa.edu.au>
2026-04-06 16:40:40 +0800

    Added Scene Changing
    Added changing between scenes and finalized greyboxes, added the GameManager script and changed some folders around

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    public void Quit()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
+
